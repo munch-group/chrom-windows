67d0d25f08511afbdb0a0a6eb9cbfe98103eb879
Kasper Munch <kaspermunch@birc.au.dk>
2025-09-19 12:25:03 +0200

    Initializing gh-pages branch

---
+++ test/test_modulename.py
@@ -0,0 +1,13 @@
+import unittest
+
+from chrom_windows.modulename import functionname
+
+class TestModulename(unittest.TestCase):
+
+    def test_functionname(self):
+        self.assertEqual(
+            functionname(
+                1, 1
+                ),
+                2
+            )
